69480f87efff6447e78b73e5fa1eaeebb55dc168
Leon <tahjib.leon399@gmail.com>
2023-08-11 20:08:29 +0600

    problem 2

--- problem-2/problem2.js
+++ problem-2/problem2.js
@@ -1,28 +1,11 @@
-// Given an array of integers, update every element with the multiplication of previous and next elements with the following exceptions.
-
-// a) The First element is replaced by the multiplication of the first and second.
-// b) The last element is replaced by multiplication of the last and second last.
-
-function updateArr(array) {
-    if (array.length <= 1) {
-        return 'array is too short';
-    }
-
-    const updatedArray = [];
-    // for first element 
-    updatedArray.push(array[0] * array[1]);
-
-    // for middle elements 
-    for (let i = 1; i < array.length - 1; i++) {
-        updatedArray.push(array[i - 1] * array[i + 1]);
-    }
-
-    // for last element 
-    updatedArray.push(array[array.length - 2] * array[array.length - 1]);
-
-    // return updateArray;
-    console.log(updatedArray);
+function totalSum(...param) {
+    // return param.reduce((a, b) => a + b);  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_reduce
+    let total = 0;
+    param.forEach(element => {
+        // total = total + element;
+        total += element
+    })
+    return total
 }
 
-const inputArray = [2, 3, 4, 5, 6];
-updateArr(inputArray);
+console.log(totalSum(1, 2, 3, 4, 54))
